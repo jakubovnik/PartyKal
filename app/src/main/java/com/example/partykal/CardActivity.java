44853f4848a5c33b5e70588553caca1679288c3e
Jakub Lukac <j.lukac152@gmail.com>
2024-04-02 15:05:44 +0200

    created card checking system and made point adding after finished_card change

--- app/src/main/java/com/example/partykal/CardActivity.java
+++ app/src/main/java/com/example/partykal/CardActivity.java
@@ -1,6 +1,8 @@
 package com.example.partykal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -11,7 +13,8 @@ public class CardActivity extends AppCompatActivity {
     TextView tv_card_name;
     TextView tv_card_description;
     TextView tv_card_points;
-    boolean card_finished;
+    ConstraintLayout cl_card;
+    boolean card_finished = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +26,7 @@ public class CardActivity extends AppCompatActivity {
         tv_card_name = findViewById(R.id.tv_card_name);
         tv_card_description = findViewById(R.id.tv_card_description);
         tv_card_points = findViewById(R.id.tv_card_points);
+        cl_card = findViewById(R.id.cl_card);
         changeCard();
     }
     public void addTenPoints(View view){
@@ -34,12 +38,30 @@ public class CardActivity extends AppCompatActivity {
         changeCard();
     }
     public void changeCard(){
+        if(card_finished){
+            PM.addPoints(this, Integer.parseInt(tv_card_points.getText().toString()));
+            String temp_string = getResources().getString(R.string.tv_points) + PM.getPoints(this);
+            tv_points.setText(temp_string);
+        }
+        setCardStateOff();
         Card card = dbm.getRandomCard();
         tv_card_name.setText(card.title);
         tv_card_description.setText(card.description);
         tv_card_points.setText(Integer.toString(card.points));
     }
+    public void setCardStateOn(){
+        cl_card.setBackground(getDrawable(R.drawable.card_border_on));
+        card_finished = true;
+    }
+    public void setCardStateOff(){
+        cl_card.setBackground(getDrawable(R.drawable.card_border_off));
+        card_finished = false;
+    }
     public void switchCardState(View view){
-
+        if(!card_finished){
+            setCardStateOn();
+        }else{
+            setCardStateOff();
+        }
     }
 }
